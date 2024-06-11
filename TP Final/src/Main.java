d152087db0f4a0c18408052075e69fe5b414e3f0
SirChurrask <LincedeMordor@gmail.com>
2024-06-11 10:48:50 -0300

    menus

--- TP Final/src/Main.java
+++ TP Final/src/Main.java
@@ -1,5 +1,112 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scan;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int seleccion = 0;
+        int i = 0;
+
+        scan = new Scanner(System.in);
+        Menu menues = new Menu();
+
+        do
+        {
+            System.out.printf(menues.mostrarMenuInicial());
+            System.out.printf("Ingrese una opcion: ");
+            seleccion = scan.nextInt();
+
+            switch (seleccion)
+            {
+                case 1:
+                    for(i=0; i<10; i++)
+                    {
+                        System.out.println("");
+
+
+                    }
+
+                    break;
+                case 2:
+                    for(i=0; i<10; i++)
+                    {
+                        System.out.println("");
+                    }
+
+                    int seleccionInstrucciones = 0;
+
+                    do
+                    {
+                        MenuInstrucciones instrucciones = new MenuInstrucciones();
+
+                        System.out.println(menues.mostrarMenuInstrucciones());
+                        System.out.printf("Ingrese una opcion: ");
+                        seleccionInstrucciones = scan.nextInt();
+
+                        switch (seleccionInstrucciones)
+                        {
+                            case 1:
+                                for(i=0; i<10; i++)
+                                {
+                                    System.out.println("");
+                                }
+
+                                System.out.println(instrucciones.mostrarJugadas());
+
+                                break;
+                            case 2:
+                                for(i=0; i<10; i++)
+                                {
+                                    System.out.println("");
+                                }
+
+                                System.out.println(instrucciones.mostrarValores());
+
+                                System.out.printf("Presione 1 para volver al menu anterior o presiones 2 para cerrar el programa: ");
+                                int opcion = scan.nextInt();
+
+                                if(opcion == 1)
+                                {
+                                    seleccionInstrucciones = 0;
+                                }
+                                break;
+                            case 3:
+                                for(i=0; i<10; i++)
+                                {
+                                    System.out.println("");
+                                }
+
+                                seleccion = 0;
+
+                                break;
+                            default:
+                                for(i=0; i<10; i++)
+                                {
+                                    System.out.println("");
+                                }
+
+                                System.out.printf("Opcion ingresada invalida, vuelvalo a intentar");
+                                break;
+                        }
+                    }while(seleccionInstrucciones < 1 || seleccionInstrucciones > 3);
+
+                    break;
+                case 3:
+                    for(i=0; i<10; i++)
+                    {
+                        System.out.println("");
+                    }
+
+                    break;
+                default:
+                    for(i=0; i<10; i++)
+                    {
+                        System.out.println("");
+                    }
+
+                    System.out.printf("Opcion ingresada invalida, vuelvalo a intentar");
+                    break;
+            }
+        }while(seleccion < 1 || seleccion > 3);
     }
 }
---
+++ TP Final/src/MenuInstrucciones.java
@@ -0,0 +1,26 @@
+public class MenuInstrucciones {
+
+    public String mostrarValores()
+    {
+        return  "Ordenados de la carta de mas valor a la de menor valor quedara de la siguiente manera:\n" +
+                "1: 1 de espada\n" +
+                "2: 1 de basto\n" +
+                "3: 7 de espada\n" +
+                "4: 7 de oro\n" +
+                "5: 3 de cada palo\n" +
+                "6: 2 de cada palo\n" +
+                "7: el 1 de oro y copa\n" +
+                "8: 12 de cada palo\n" +
+                "9: 11 de cada palo\n" +
+                "10: 10 de cada palo\n" +
+                "11: el 7 de copa y basto\n" +
+                "12: 6 de cada palo\n" +
+                "13: 5 de cada palo\n" +
+                "14: 4 de cada palo\n";
+    }
+
+    public String mostrarJugadas()
+    {
+        return "";
+    }
+}
